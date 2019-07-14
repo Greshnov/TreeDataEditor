6a1d89126cfa9b1c92cc0e5c9cf5436517aeea7c
Greshnov <gram-73@mail.ru>
2019-07-15 01:46:55 +0400

    Добавлены поля ввода/вывода информации выделенной строки в виде отдельного блока справа от списка контрактов. Добавлено место под блок фильтров.

--- TreeDataEditor/MainWindow.xaml.cs
+++ TreeDataEditor/MainWindow.xaml.cs
@@ -14,6 +14,70 @@ namespace TreeDataEditor
             
             // Тестовые данные
             DataContext = new { Contracts = TestData.Contracts };
+
+        /// <summary>
+        /// Обработчик выбора строки в списке контрактов
+        /// </summary>
+        private void ContractsList_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            ClearFields();
+
+            var item = ContractsList.SelectedItem;
+
+            if (item is Contract)
+            {
+                var contract = (Contract)item;
+                ContractFullName.Text = contract.FullName;
+                ContractNumber.Text = contract.ContractNumber.ToString();
+                Customer.Text = contract.Customer;
+                CommercialNetwork.Text = contract.CommercialNetwork;
+                Status.Text = contract.Status;
+                StartDate.Text = contract.StartDate.ToShortDateString();
+                EndDate.Text = contract.EndDate.ToShortDateString();
+            }
+            else if (item is Product)
+            {
+                var product = (Product)item;
+                ProductName.Text = product.Name;
+                RetroSum.Text = product.Retro.HasValue ? product.Retro.ToString() : null;
+                RetroDistrSum.Text = product.RetroDistr.HasValue ? product.RetroDistr.ToString() : null;
+                RkuSum.Text = product.Rku.HasValue ? product.Rku.ToString() : null;
+                RkuDistrSum.Text = product.RkuDistr.HasValue ? product.RkuDistr.ToString() : null;
+            }
+            else if (item is ConditionKind)
+            {
+                var condition = (ConditionKind)item;
+                ConditionKindName.Text = condition.Name;
+                Retro.Text = condition.Retro.HasValue ? condition.Retro.ToString() : null;
+                RetroDistr.Text = condition.RetroDistr.HasValue ? condition.RetroDistr.ToString() : null;
+                Rku.Text = condition.Rku.HasValue ? condition.Rku.ToString() : null;
+                RkuDistr.Text = condition.RkuDistr.HasValue ? condition.RkuDistr.ToString() : null;
+
+            }
         }
+
+        /// <summary>
+        /// Очищает поля ввода / вывода
+        /// </summary>
+        void ClearFields()
+        {
+            ContractFullName.Text = null;
+            ContractNumber.Text = null;
+            Customer.Text = null;
+            CommercialNetwork.Text = null;
+            Status.Text = null;
+            StartDate.Text = null;
+            EndDate.Text = null;
+            ProductName.Text = null;
+            RetroSum.Text = null;
+            RetroDistrSum.Text = null;
+            RkuSum.Text = null;
+            RkuDistrSum.Text = null;
+            Retro.Text = null;
+            RetroDistr.Text = null;
+            Rku.Text = null;
+            RkuDistr.Text = null;
+        }
+
     }
 }
